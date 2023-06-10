f8bab45c89096070900eb76221684233b7cb5676
markeybass <markeybass@gmail.com>
2023-06-10 22:17:05 +0300

    added hipos pic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import hipos from "./hipos.jpg"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload...❗❗❗ Yoo hoo❗❗❗ 
         </p>
+        <img style={{maxWidth: "500px"}} src={hipos}></img>
         <a
           className="App-link"
           href="https://reactjs.org"
